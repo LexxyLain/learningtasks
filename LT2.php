3c140eb43d3875aac30b2a5f020e9bbb26e3a39e
LexxyLain <100071781+LexxyLain@users.noreply.github.com>
2022-02-20 23:59:50 +0800

    Update and rename LT2.html to LT2.php

---
+++ LT2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Learning Task 2</title>
+    </head>
+    <h1>Recipes</h1>
+    <li><a href="Chili con Carne.php">Chili con Carne</a></li>
+    <li><a href="Thai Green Curry.php">Thai Green Curry</a></li>
+    <li><a href="Pineapple Chicken.php">Pineapple Chicken</a></li>
+    <li><a href="Omelette.php">Omelette</a></li>
+    <li><a href="Egg Fried Rice.php">Egg Fried Rice</a></li>
+    <li><a href="Salmon in the Oven.php">Salmon in the Oven</a></li>
+    <li><a href="Basic Pasta.php">Basic Pasta</a></li>
+</html>
