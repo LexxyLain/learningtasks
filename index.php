1a08b608fb7b64b11561972309341d998b85685b
LexxyLain <100071781+LexxyLain@users.noreply.github.com>
2022-02-21 00:21:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <h1> Welcome to My Website! I am Lexxy Lain Villa! </h1>
 <ul>
   <li><a href="index.php">HOME</a></li>
-  <li><a href="/LT2/index.html">LEARNING TASK 2</a><
+  <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
 <ul>
 </body>
   </html>
