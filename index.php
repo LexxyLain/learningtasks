625d7eeb3eb4ba52a39ebc549e8cded284548ce7
LexxyLain <100071781+LexxyLain@users.noreply.github.com>
2022-02-21 00:08:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
-   <head>
-       <title> My Home Page  </title>
-   </head>
-    <h1>Welcome to my Website! I am Lexxy Lain Villa!</h1>  
-    <h2><li><a href="index.php">Home</a></li></h2>
-    <h2><li><a href="LT2.php">My Learning Task 2</a></li></h2>
-</html>
+<head>
+<title> My Home Page </title>
+</head>
+<body>
+<h1> Welcome to My Website! I am Lexxy Lain Villa! </h1>
+<ul>
+  <li><a href="index.php">HOME</a></li>
+  <li><a href="/LT2/index.html">LEARNING TASK 2</a><
+<ul>
+</body>
+</html
