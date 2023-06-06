c58cbe7866c33254cca5f931f336579eb6976d46
LexxyLain <100071781+LexxyLain@users.noreply.github.com>
2023-06-06 22:43:13 +0800

    Delete JP directory

--- JP/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'config/config.php';
-session_destroy();
-header("location: index.php");
--- JP/room-module/db_rooms.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `tbl_rooms`;
-CREATE TABLE `tbl_rooms` (
-    `room_id` int(8) unsigned NOT NULL auto_increment,
-    `room_price` varchar(180) NOT NULL default '',
-    `room_status` varchar(180) NOT NULL default '',
-    PRIMARY KEY (`room_id`)
-
-) ENGINE=MyISAM AUTO_INCREMENT=10000001;
